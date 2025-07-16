f620eea06ab0edb5ec0d58f2768b41643724868c
juto-shogan <somtombonu53@gmail.com>
2025-07-16 15:59:40 +0100

    set

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-import  'express'
+const express = require('express');
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT,() => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+
